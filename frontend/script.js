7f3f9a1dbb92ef4c054950620a4aca1f5d2d7c93
dnezero <micheleminecraft32@hotmail.com>
2025-06-10 11:33:55 +0200

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // URL of your Flask backend server
     // Ensure your Flask backend is running and accessible at this URL
-    const BACKEND_BASE_URL = 'http://InnovaChatBackEnd.onrender.com/'; 
+    const BACKEND_BASE_URL = 'https://InnovaChatBackEnd.onrender.com/'; 
 
     // HTML element references - Chat interface
     const messagesDisplay = document.getElementById('messages-display');
